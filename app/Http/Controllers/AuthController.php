19f3f3b3cab759aedb25b7314c150e3bb68f87d0
El-karece Asiedu <55034413+KareceA@users.noreply.github.com>
2023-07-11 11:05:36 +0000

    Update AuthController.php
    logout function in Auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function logout(Request $request)
+    {
+        try {
+            $request->user()->currentAccessToken()->delete();
+
+            return response()->json(['message' => 'Logged out successfully']);
+        } catch (\Exception $exception) {
+
+            return response()->json(['error' => 'An error occured'], 500);
+        }
+
+    }
 }
